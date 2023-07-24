e4c3df4ad28177ea18db9417517424635e981fb2
DEntisT <zukanoviceltarik>
2023-07-24 15:02:00 +0200

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,22 @@
 
 #include "utils.cpp"
 
+class testClass
+{
+public:
+	int var = 23;
+	
+};
+
 int main()
 {
 	using namespace utils;
-	
+
 	echo("Test");
 	echorectangle(4,4,'i',false);
 	std::cout << strcomp("ee", "hi");
+
+	testClass variables;
+	std::cout << variables.var;
 	return 0;
 }
